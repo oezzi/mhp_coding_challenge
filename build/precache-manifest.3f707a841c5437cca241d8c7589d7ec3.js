51435d09a5fba0053f4826c7ff8264753fee0cfa
oezguer demir <oezguerdemir@web.de>
2020-08-17 14:27:20 +0200

    latest build

---
+++ build/precache-manifest.3f707a841c5437cca241d8c7589d7ec3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c284437fff2f7160b7082a45142b5ef1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2bff077df66956a27e45",
+    "url": "/static/css/main.11311567.chunk.css"
+  },
+  {
+    "revision": "51d2bd7d872905c9b371",
+    "url": "/static/js/2.5d02e227.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.5d02e227.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bff077df66956a27e45",
+    "url": "/static/js/main.7f94d5a3.chunk.js"
+  },
+  {
+    "revision": "5afe275f78566d07a9a4",
+    "url": "/static/js/runtime-main.dfc2486a.js"
+  }
+]);
